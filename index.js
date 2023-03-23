6131866dfd7081ad45f5afbb455cf616262536eb
Assar <assarlannerbornzz@gmail.com>
2023-03-23 10:44:26 +0100

    Added version 1

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+for (let index = 0; index < 200; index++) {
+    
+    const r = Math.random()*255;
+    const g = Math.random()*255;
+    const b = Math.random()*255;
+
+    r.toString();
+    g.toString();
+    b.toString();
+
+    console.log("rgb("+r+","+g+","+b+")");
 
 
-
-document.getElementById("box").addEventListener("click", (event)=> {
-
-    alert("yo");
-
-})
+    const element = document.createElement("div");
+    element.classList.add("small-box");
+    element.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+    document.querySelector("body").appendChild(element);
+    
+}
